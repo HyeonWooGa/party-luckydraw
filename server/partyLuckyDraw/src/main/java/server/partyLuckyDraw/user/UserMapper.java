2581fcfe96f2ec3cd5906f0114e37a0ce55fe546
시온백 <tldhs05222@naver.com>
2022-12-14 23:17:29 +0900

    feat : userMapper 로직 작성

---
+++ server/partyLuckyDraw/src/main/java/server/partyLuckyDraw/user/UserMapper.java
@@ -0,0 +1,18 @@
+package server.partyLuckyDraw.user;
+
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.ReportingPolicy;
+
+@Mapper(componentModel = "spring", typeConversionPolicy = ReportingPolicy.IGNORE)
+public interface UserMapper {
+    @Mapping(source = "userName.userName", target = "name")
+    User toUser(UserDto.userName userName);
+
+    @Mapping(source = "user.id", target = "userId")
+    UserDto.ResponseUserId toUserId(User user);
+
+    @Mapping(source = "user.name", target = "userName")
+    @Mapping(source = "user.id", target = "userId")
+    UserDto.ResponseSimpleUser toSimpleUser(User user);
+}
