b0b6d8ae8071fc9abf54ce8bc939955fe2cf9604
시온백 <tldhs05222@naver.com>
2022-12-14 23:17:54 +0900

    feat : userService 로직 작성

---
+++ server/partyLuckyDraw/src/main/java/server/partyLuckyDraw/user/UserService.java
@@ -0,0 +1,33 @@
+package server.partyLuckyDraw.user;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+@Transactional
+@RequiredArgsConstructor
+public class UserService {
+
+    private final UserRepository userRepository;
+    private final UserMapper userMapper;
+
+    public UserDto.ResponseUserId registerUserName(UserDto.userName userName) {
+        User user = userMapper.toUser(userName);
+        userRepository.save(user);
+        return userMapper.toUserId(user);
+    }
+
+    public List<UserDto.ResponseSimpleUser> getUsers() {
+        return userRepository.findAll().stream()
+                .map(userMapper::toSimpleUser)
+                .collect(Collectors.toList());
+    }
+
+    public void deleteUser(Long userId) {
+        userRepository.deleteById(userId);
+    }
+}
