0b1fc0b82dc0b0e7128df9f652616481c5d5ab70
시온백 <tldhs05222@naver.com>
2022-12-15 00:13:42 +0900

    refactor : userMapper userName -> username으로 변수명 변경

--- server/partyLuckyDraw/src/main/java/server/partyLuckyDraw/user/UserMapper.java
+++ server/partyLuckyDraw/src/main/java/server/partyLuckyDraw/user/UserMapper.java
@@ -6,13 +6,13 @@ import org.mapstruct.ReportingPolicy;
 
 @Mapper(componentModel = "spring", typeConversionPolicy = ReportingPolicy.IGNORE)
 public interface UserMapper {
-    @Mapping(source = "userName.userName", target = "name")
-    User toUser(UserDto.RequestUserSimpleUser userName);
+    @Mapping(source = "username.username", target = "name")
+    User toUser(UserDto.RequestUserSimpleUser username);
 
     @Mapping(source = "user.id", target = "userId")
     UserDto.ResponseUserId toUserId(User user);
 
-    @Mapping(source = "user.name", target = "userName")
+    @Mapping(source = "user.name", target = "username")
     @Mapping(source = "user.id", target = "userId")
-    UserDto.ResponseSimpleUser toSimpleUser(User user);
+    UserDto.ResponseSimpleUser toSimpleUserDto(User user);
 }
