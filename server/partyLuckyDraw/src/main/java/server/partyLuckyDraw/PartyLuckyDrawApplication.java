bee9759607d8e3e1dc84fb66f86e6768d4f026ad
시온백 <104377048+SionBackEnd@users.noreply.github.com>
2022-12-14 22:05:05 +0900

    build : 백엔드 초기 파일 세팅

---
+++ server/partyLuckyDraw/src/main/java/server/partyLuckyDraw/PartyLuckyDrawApplication.java
@@ -0,0 +1,13 @@
+package server.partyLuckyDraw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PartyLuckyDrawApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PartyLuckyDrawApplication.class, args);
+	}
+
+}
---
+++ server/partyLuckyDraw/src/test/java/server/partyLuckyDraw/PartyLuckyDrawApplicationTests.java
@@ -0,0 +1,13 @@
+package server.partyLuckyDraw;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PartyLuckyDrawApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
