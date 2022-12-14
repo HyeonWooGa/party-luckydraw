746cf4e536650f3533b2348e6fddb5a4860b6690
시온백 <tldhs05222@naver.com>
2022-12-15 00:14:06 +0900

    feat : userRepository.findByName 로직 작성

--- server/partyLuckyDraw/src/main/java/server/partyLuckyDraw/user/UserRepository.java
+++ server/partyLuckyDraw/src/main/java/server/partyLuckyDraw/user/UserRepository.java
@@ -2,6 +2,11 @@ package server.partyLuckyDraw.user;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<List<User>> findByName(String userName);
 }
 
