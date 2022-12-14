d74f08a0050ae5de73cc57c47a7a92b814ed3358
시온백 <tldhs05222@naver.com>
2022-12-14 23:18:03 +0900

    feat : userDto 로직 작성

---
+++ server/partyLuckyDraw/src/main/java/server/partyLuckyDraw/user/UserDto.java
@@ -0,0 +1,32 @@
+package server.partyLuckyDraw.user;
+
+import lombok.*;
+
+
+public class UserDto {
+
+    @Getter
+    @Builder
+    @NoArgsConstructor(access = AccessLevel.PROTECTED)
+    @AllArgsConstructor(access = AccessLevel.PROTECTED)
+    public static class userName {
+        private String userName;
+    }
+
+    @Getter
+    @Builder
+    @NoArgsConstructor(access = AccessLevel.PROTECTED)
+    @AllArgsConstructor(access = AccessLevel.PROTECTED)
+    public static class ResponseUserId {
+        private Long userId;
+    }
+
+    @Getter
+    @Builder
+    @NoArgsConstructor(access = AccessLevel.PROTECTED)
+    @AllArgsConstructor(access = AccessLevel.PROTECTED)
+    public static class ResponseSimpleUser {
+        private Long userId;
+        private String userName;
+    }
+}
