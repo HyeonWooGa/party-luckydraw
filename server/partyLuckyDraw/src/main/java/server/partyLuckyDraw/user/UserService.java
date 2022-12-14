649592d7bd1f5e9ebe1d80013edd691542d6f575
시온백 <tldhs05222@naver.com>
2022-12-15 00:15:00 +0900

    feat : userService 램덤으로 유저 선택로직과 저장된 유저 이름으로 찾아내는 로직 작성

--- server/partyLuckyDraw/src/main/java/server/partyLuckyDraw/user/UserService.java
+++ server/partyLuckyDraw/src/main/java/server/partyLuckyDraw/user/UserService.java
@@ -15,19 +15,34 @@ public class UserService {
     private final UserRepository userRepository;
     private final UserMapper userMapper;
 
-    public UserDto.ResponseUserId registerUserName(UserDto.RequestUserSimpleUser userName) {
-        User user = userMapper.toUser(userName);
+    public UserDto.ResponseUserId registerUserName(UserDto.RequestUserSimpleUser username) {
+        User user = userMapper.toUser(username);
         userRepository.save(user);
         return userMapper.toUserId(user);
     }
 
     public List<UserDto.ResponseSimpleUser> getUsers() {
         return userRepository.findAll().stream()
-                .map(userMapper::toSimpleUser)
+                .map(userMapper::toSimpleUserDto)
                 .collect(Collectors.toList());
     }
 
     public void deleteUser(Long userId) {
         userRepository.deleteById(userId);
+    }
+
+    public UserDto.ResponseSimpleUser findRandomPickUser() {
+        List<User> users = userRepository.findAll();
+        // 빈 리스트면 예외 터트리기
+        int luckyUserId = (int) (Math.random() * users.size());
+        User user = users.get(luckyUserId);
+        return userMapper.toSimpleUserDto(user);
+    }
+
+    public List<UserDto.ResponseSimpleUser> checkSavedUserByName(UserDto.RequestUsername requestUsername) {
+        List<User> users = userRepository.findByName(requestUsername.getUsername()).orElseThrow();
+        return users.stream()
+                .map(userMapper::toSimpleUserDto)
+                .collect(Collectors.toList());
     }
 }
