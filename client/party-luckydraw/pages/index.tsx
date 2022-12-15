64ded794ef321625c6fc8f7aa968b2abf7677c08
HyeonWooGa <yeonwoopark22@gmail.com>
2022-12-15 15:26:23 +0900

    style: phone 정보 삭제 및 텍스트 크기 조절

--- client/party-luckydraw/pages/index.tsx
+++ client/party-luckydraw/pages/index.tsx
@@ -26,8 +26,8 @@ const divVariants = {
       type: "spring",
       duration: 1,
       bounce: 0.5,
-      delayChildren: 1,
-      staggerChildren: 1,
+      delayChildren: 1.2,
+      staggerChildren: 1.2,
     },
   },
 };
@@ -63,8 +63,8 @@ const Home: NextPage = () => {
     setLayoutId("1");
   };
 
-  const onClickDelete = (userId: number, userName: string, phone: string) => {
-    deleteParticipant(userId, userName, phone, setRendering);
+  const onClickDelete = (userId: number, userName: string) => {
+    deleteParticipant(userId, userName, setRendering);
   };
   return (
     <motion.main className="w-[1080px] mx-auto my-8 flex flex-col gap-10">
@@ -85,14 +85,13 @@ const Home: NextPage = () => {
             required
           />
         </div>
-        <div className="flex items-center">
+        <div className="hidden">
           <span className="w-[574px]">핸드폰 뒷번호 : </span>
           <input
             value={phone}
             onChange={(e) => setPhone(e.target.value)}
             className="w-[440px] rounded-xl px-4 py-2"
             placeholder="4자리수"
-            required
           />
         </div>
         <button className="hidden">제출</button>
@@ -102,14 +101,12 @@ const Home: NextPage = () => {
           {participants?.map((participant) => (
             <button
               onClick={() =>
-                onClickDelete(
-                  participant.userId,
-                  participant.username,
-                  participant.phone
-                )
+                onClickDelete(participant.userId, participant.username)
               }
-              className="bg-[#E8C4C4] py-2 px-4 rounded-xl"
-            >{`${participant.username} (${participant.phone})`}</button>
+              className="bg-[#E8C4C4] py-2 px-4 rounded-xl text-4xl"
+            >
+              {participant.username}
+            </button>
           ))}
         </div>
       </section>
@@ -128,25 +125,27 @@ const Home: NextPage = () => {
         {layoutId ? (
           <motion.div
             onClick={() => setLayoutId("")}
-            initial={{ backgroundColor: "rgba(0,0,0,0)" }}
-            animate={{ backgroundColor: "#2B3A55" }}
-            exit={{ backgroundColor: "rgba(0,0,0,0)" }}
+            initial={{
+              color: "#F2E5E5",
+              backgroundColor: "rgba(0,0,0,0)",
+            }}
+            animate={{ color: "#F2E5E5", backgroundColor: "#2B3A55" }}
             className="w-screen h-screen fixed left-0 bottom-0 flex justify-center items-center"
           >
             <motion.section
-              className="text-[#F2E5E5] flex flex-col gap-12 items-center"
+              className="flex flex-col gap-12 items-center"
               layoutId={layoutId}
             >
-              <div className="text-4xl">축하합니다! 당첨자는!?</div>
+              <div className="text-8xl font-medium">축하합니다! 당첨자는!?</div>
               <motion.div variants={divVariants} initial="start" animate="end">
                 {personWonDraw.split("").map((char) => (
-                  <motion.span variants={spanVariants} className="text-8xl">
-                    {char}
+                  <motion.span
+                    variants={spanVariants}
+                    className="text-[200px] font-extrabold"
+                  >
+                    {`${char} `}
                   </motion.span>
                 ))}
-                <motion.span variants={spanVariants} className="text-8xl">
-                  {` (${phoneWonDraw})`}
-                </motion.span>
               </motion.div>
             </motion.section>
           </motion.div>
