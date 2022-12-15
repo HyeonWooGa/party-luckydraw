eba3ff799a700439c84388652063821e5e25bdf7
HyeonWooGa <yeonwoopark22@gmail.com>
2022-12-15 14:00:55 +0900

    chore: 프록시 설정

--- client/party-luckydraw/next.config.js
+++ client/party-luckydraw/next.config.js
@@ -2,6 +2,15 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
+  async rewrites() {
+    return [
+      {
+        source: "/:path*",
+        destination:
+          "http://ec2-13-125-113-82.ap-northeast-2.compute.amazonaws.com:8000/:path*",
+      },
+    ];
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
